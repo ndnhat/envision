ce4664cdf62c645b7d3fa385afff03866152635f
Nhat Nguyen <ndnhat@hotmail.com>
2014-06-10 15:25:28 -0600

    refactor validate method

--- routes/validate.js
+++ routes/validate.js
@@ -1,35 +1,45 @@
 var gm = require('gm');
 
+function getError(code) {
+  var error = { statusCode: code };
+  error.message = (code === 400)
+                ? 'The "image" parameter is required'
+                : 'Something is messed up';
+  return error;
+}
+
 module.exports = function (app) {
   app.get('/validate', function(req, res) {
-    var data = { valid: false };
+    var data = { valid: true };
     var imagePath = req.query.image;
     var image;
     
-    if (!imagePath) {
-      data.error = {
-        statusCode: 400,
-        message: 'The "image" parameter is required'
-      };
-      res.json(data);
+    if (imagePath) {
+      image = gm(imagePath);
+    } else {
+      res.json({error: getError(400)});
       return;
     }
 
-    gm(imagePath).size(function (err, size) {
-      if (!err) {
-        var minWidth = parseInt(req.query['min-width'], 10) || 0;
-        var minHeight = parseInt(req.query['min-height'], 10) || 0;
-        var maxWidth = parseInt(req.query['max-width'], 10) || Infinity;
-        var maxHeight = parseInt(req.query['max-height'], 10) || Infinity;
+    if (req.query['min-width'] || req.query['min-height'] || req.query['max-width'] || req.query['max-height']) {
+      image.size(function (err, size) {
+        if (!err) {
+          var minWidth = parseInt(req.query['min-width'], 10) || 0;
+          var minHeight = parseInt(req.query['min-height'], 10) || 0;
+          var maxWidth = parseInt(req.query['max-width'], 10) || Infinity;
+          var maxHeight = parseInt(req.query['max-height'], 10) || Infinity;
 
-        data.size = size;
-        data.valid = size.width >= minWidth &&
-                     size.height >= minHeight &&
-                     size.width <= maxWidth &&
-                     size.height <= maxHeight;
-      }
-
-    });
+          data.size = size;
+          data.valid = data.vaild &&
+                       size.width >= minWidth &&
+                       size.height >= minHeight &&
+                       size.width <= maxWidth &&
+                       size.height <= maxHeight;
+        } else {
+          data.error = getError(500);
+        }
+      });
+    }
     res.json(data);
 
   });
