6cc48adda76f2f90d6ee2f079caed84ba2340473
Nhat Nguyen <ndnhat@hotmail.com>
2014-06-12 23:55:26 -0600

    add test case for image type

--- test/size.js
+++ test/size.js
@@ -12,11 +12,12 @@ describe('Validate', function() {
       }
       res.body.should.have.property('error');
       res.body.error.should.have.status(400);
+      res.body.should.be.valid;
       done();
     });
   });
 
-  describe('size', function() {
+  describe('image size', function() {
     it('should return true when no constraints are present', function(done) {
       request.get('/validate?image=http%3A%2F%2Florempixel.com%2F40%2F20%2F').end(function(err, res) {
         if (err) {
@@ -73,6 +74,19 @@ describe('Validate', function() {
           throw err;
         }
         res.body.should.be.valid;
+        done();
+      });
+    });
+  });
+
+  describe('image type', function() {
+    it('should return true for a valid image/png type', function(done) {
+      request.get('/validate?image=http%3A%2F%2Florempixel.com%2F40%2F20%2F&mimetype=image/png').end(function(err, res) {
+        if (err) {
+          throw err;
+        }
+        res.body.should.be.valid;
+        res.body.type.should.eql('png');
         done();
       });
     });
