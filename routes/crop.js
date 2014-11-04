175839fadb0daa3a4f0c915d7502cb37495b83d3
Nhat Nguyen <ndnhat@hotmail.com>
2014-11-04 15:13:19 -0700

    refactor image manipulation

--- routes/crop.js
+++ routes/crop.js
@@ -7,34 +7,22 @@ var upload = require('./s3-upload');
 function crop(req, res) {
   try {
     var q = req.query;
-    var filename = getFilename(q.image);
-    var image = gm(request(q.image), filename);
+    var image = gm(request(q.image)).crop(q.width, q.height, q.left, q.top);
 
-    image.crop(q.width, q.height, q.left, q.top).stream(function(err, stdout, stderr) {
-      var buf = new Buffer(0);
+    image.toBuffer(function(err, buffer) {
+      var fileObj = {
+        alteration: 'cropped',
+        protocol: req.protocol,
+        prefix: q.prefix,
+        buffer: buffer,
+        mimetype: mime.lookup(image.source)
+      };
 
-      stdout.on('data', function(d) {
-        buf = Buffer.concat([buf, d]);
-      });
-
-      stdout.on('end', function() {
-        var fileObj = {
-          alteration: 'cropped',
-          protocol: req.protocol,
-          prefix: q.prefix,
-          buffer: buf,
-          mimetype: mime.lookup(filename)
-        };
-        upload(fileObj, res);
-      });
+      upload(fileObj, res);
     });
   } catch(e) {
     res.status(500).send(e);
   }
-}
-
-function getFilename(url) {
-  return url.substring(url.lastIndexOf('/')+1);
 }
 
 module.exports = function (app) {
--- routes/rotate.js
+++ routes/rotate.js
@@ -6,32 +6,22 @@ var upload = require('./s3-upload');
 
 function rotate(req, res) {
   try {
-    var q = req.query;
-    var image = gm(request(q.image));
-    image.rotate('white', q.degrees).stream(function(err, stdout, stderr) {
-      var buf = new Buffer(0);
+    var image = gm(request(req.query.image)).rotate('white', req.query.degrees);
 
-      stdout.on('data', function(d) {
-        buf = Buffer.concat([buf, d]);
-      });
+    image.toBuffer(function(err, buffer) {
+      var fileObj = {
+        alteration: 'rotated',
+        protocol: req.protocol,
+        prefix: req.query.prefix,
+        buffer: buffer,
+        mimetype: mime.lookup(image.source)
+      };
 
-      stdout.on('end', function() {
-        var fileObj = {
-          alteration: 'rotated',
-          protocol: req.protocol,
-          prefix: q.prefix,
-          buffer: buf
-        };
-        upload(fileObj, res);
-      });
+      upload(fileObj, res);
     });
   } catch(e) {
     res.status(500).send(e);
   }
-}
-
-function getFilename(url) {
-  return url.substring(url.lastIndexOf('/')+1);
 }
 
 module.exports = function (app) {
