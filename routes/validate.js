cc0337bab4dc370f42d61436ab17f99428403349
Nhat Nguyen <ndnhat@hotmail.com>
2014-12-23 10:34:26 -0700

    refactor validate route

--- routes/validate.js
+++ routes/validate.js
@@ -1,49 +1,55 @@
 var gm = require('gm');
-var async = require('async');
 var request = require('request');
 var cors = require('cors');
 
 function validate(req, res) {
-  try {
-    var image = getImage(req.query.image);
-    validateImage(req, res, image);
-  } catch(e) {
-    error(res, e);
+  if (!req.query.image) {
+    error('The "image" parameter is required.', res);
+  } else {
+    verifyImage(req, res, validateImage);
   }
 }
 
-function validateImage(req, res, img) {
-  var tasks = [];
-  if (req.query['min-width'] || req.query['min-height'] || req.query['max-width'] || req.query['max-height']) {
-    tasks.push(validateSize.bind(img, req.query));
-  }
+function verifyImage(req, res, next) {
+  var path = req.query.image;
+  request.head(path, function(err, response, body) {
+    if (err) {
+      error(err, res);
+    } else if (response.headers['content-type'].indexOf('image') < 0) {
+      error('The file provided is not an image', res);
+    } else {
+      next(req, res);
+    }
+  });
 
-  if (req.query['mimetype']) {
-    tasks.push(validateType.bind(img, req.query));
-  }
+}
 
-  if (tasks.length) {
-    async.parallel(
-      tasks,
-      function(err, results) {
-        if (err) {
-          throw err;
-        } else {
-          res.json(combine(results));
-        }
+function validateImage(req, res) {
+  var path = req.query.image;
+  var image = gm(request(path), 'tempImage');
+  image.identify(function(err, info) {
+    if (err) {
+      error(err, res);
+    } else {
+      var validations = [];
+      if (req.query['min-width'] || req.query['min-height'] || req.query['max-width'] || req.query['max-height']) {
+        validations.push(validateSize(info.size, req.query));
       }
-    );
-  } else {
-    res.json(valid());
-  }
+      if (req.query['mimetype']) {
+        validations.push(validateType(info.format, req.query));
+      }
 
+      res.json(combine(validations));
+    }
+  });
 }
 
 function valid() {
   return { valid: true };
 }
 
-function error(res, exception) {
+function error(err, res) {
+  var exception = new Error(err);
   res.status(500).json({
     error: {
       statusCode: 500,
@@ -52,72 +58,57 @@ function error(res, exception) {
   });
 }
 
-function getImage(path) {
-  if (!path) {
-    throw 'The "image" parameter is required.';
+function validateSize(size, query) {
+  var minWidth = parseInt(query['min-width'], 10) || 0;
+  var minHeight = parseInt(query['min-height'], 10) || 0;
+  var maxWidth = parseInt(query['max-width'], 10) || Infinity;
+  var maxHeight = parseInt(query['max-height'], 10) || Infinity;
+  var data = {};
+
+  data.info = { size: size };
+  data.valid = size.width >= minWidth &&
+               size.height >= minHeight &&
+               size.width <= maxWidth &&
+               size.height <= maxHeight;
+  if (size.width > maxWidth || size.height > maxHeight) {
+    data.invalid = {
+      size: {
+        code: 'image-size-too-large',
+        message: 'The image size is too large.'
+      }
+    };
+  } else if (size.width < minWidth || size.height < minHeight) {
+    data.invalid = {
+      size: {
+        code: 'image-size-too-small',
+        message: 'The image size is too small.'
+      }
+    };
   }
-  return gm(request(path), 'tempImage');
+
+  return data;
 }
 
-function validateSize(query, cb) {
-  this.size(function (err, size) {
-    var data = {};
-    if (!err) {
-      var minWidth = parseInt(query['min-width'], 10) || 0;
-      var minHeight = parseInt(query['min-height'], 10) || 0;
-      var maxWidth = parseInt(query['max-width'], 10) || Infinity;
-      var maxHeight = parseInt(query['max-height'], 10) || Infinity;
-
-      data.info = { size: size };
-      data.valid = size.width >= minWidth &&
-                   size.height >= minHeight &&
-                   size.width <= maxWidth &&
-                   size.height <= maxHeight;
-      if (size.width > maxWidth || size.height > maxHeight) {
-        data.invalid = {
-          size: {
-            code: 'image-size-too-large',
-            message: 'The image size is too large.'
-          }
-        };
-      } else if (size.width < minWidth || size.height < minHeight) {
-        data.invalid = {
-          size: {
-            code: 'image-size-too-small',
-            message: 'The image size is too small.'
-          }
-        };
+function validateType(type, query) {
+  var data = {};
+  data.info = { type: type.toLowerCase() };
+  data.valid = query.mimetype.indexOf(data.info.type) >= 0;
+  if (!data.valid) {
+    data.invalid = {
+      type: {
+        code: 'image-type-invalid',
+        message: 'The image format is invalid.'
       }
-    } else {
-      throw 'Unable to validate image size';
-    }
-    cb(null, data);
-  });
-}
+    };
+  }
 
-function validateType(query, cb) {
-  this.format(function (err, type) {
-    var data = {};
-    if (!err) {
-      data.info = { type: type.toLowerCase() };
-      data.valid = query.mimetype.indexOf(data.info.type) >= 0;
-      if (!data.valid) {
-        data.invalid = {
-          type: {
-            code: 'image-type-invalid',
-            message: 'The image format is invalid.'
-          }
-        };
-      }
-    } else {
-      throw 'Unable to validate image type';
-    }
-    cb(null, data);
-  });
-
+  return data;
 }
 
 function combine(results) {
+  if (!results.length) {
+    return valid();
+  }
   return results.reduce(merge);
 }
 
--- test/type.js
+++ test/type.js
@@ -6,6 +6,17 @@ describe('Validate', function() {
   request = request(process.env.HOST || 'http://localhost:5000');
 
   describe('image type', function() {
+    it('should return an error for non-image files', function(done) {
+      request.get('/validate?image=http://www.tonycuffe.com/mp3/tailtoddle_lo.mp3&mimetype=image/jpeg').end(function(err, res) {
+        if (err) {
+          throw err;
+        }
+        res.body.should.have.property('error');
+        done();
+      });
+    });
+
+
     it('should return true for a valid image/jpeg type', function(done) {
       request.get('/validate?image=http%3A%2F%2Florempixel.com%2F40%2F20%2F&mimetype=image/jpeg').end(function(err, res) {
         if (err) {
